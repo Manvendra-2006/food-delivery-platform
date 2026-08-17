353d9765903624b4d455c449c715d16a75856e68
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-08-17 16:38:12 +0530

    feat:Implement upload service using cloudinary

--- Backend/utilis/app.js
+++ Backend/utilis/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import cloudinary from 'cloudinary'
-import router from './routes/cloudinary'
+import router from './routes/cloudinary.js'
 const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json({limit:"50mb"}))
+app.use(express.urlencoded({extended:true,
+    limit:"50mb"
+}))
 
 const {CLOUD_NAME,CLOUD_API_KEY,CLOUD_SECRET_KEY} = process.env
 if(!CLOUD_NAME||!CLOUD_API_KEY||!CLOUD_SECRET_KEY){
